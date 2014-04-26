34275a768d9ac581be6e43f6b249bcbe0f344e1c
nocturnaltortoise <simonturner34@gmail.com>
2014-04-26 21:16:47 +0100

    adding source files

---
+++ src/main/Game.java
@@ -0,0 +1,115 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Game extends JPanel implements ActionListener, KeyListener{
+	
+	private Dimension windowSize = new Dimension(1024, 768);
+	public static boolean keys[] = new boolean[256];
+	private Image playerImage;
+
+	Timer timer = new Timer(20, this);
+	Player hero = new Player(windowSize.width / 2, windowSize.height / 2,  16,16, playerImage);
+	
+	public Game(){
+		
+		JFrame frame = new JFrame("Ludum Dare 29");
+		frame.setContentPane(this);
+		this.setPreferredSize(windowSize);
+		frame.pack();
+		frame.setLocation(200, 200);
+		this.setBackground(Color.BLACK);
+		
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(this);
+		frame.setVisible(true);
+		this.timer.start();
+		
+	}
+	
+	public static void main(String[] args){
+		
+		new Game();
+		
+	}
+	
+	private void update(){
+
+		hero.updatePlayer();
+		
+	}
+	
+	private Image loadTexture(String filename){
+		Image tempImg;
+		
+		try {
+			tempImg = ImageIO.read(new File(filename));
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return tempImg;
+		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g){
+		
+		
+		Graphics2D g2d = (Graphics2D)g;
+		super.paintComponent(g2d);
+		
+		playerImage = loadTexture("res/Man.png");
+		g2d.drawImage(playerImage, hero.getPlayerDimensions().x, hero.getPlayerDimensions().y, null);
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		keys[e.getKeyCode()] = true;
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+		keys[e.getKeyCode()] = false;
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == timer){
+			
+			update();
+			repaint();
+			
+		}
+		
+		
+	}
+	
+}
---
+++ src/main/Player.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import java.io.File;
+
+public class Player {
+
+	private Rectangle playerDimensions = new Rectangle(0,0,0,0);
+	//private File playerImage = new File("/res/Man.png");
+	private Image playerImage;
+
+	public Rectangle getPlayerDimensions(){
+		return playerDimensions;
+	}
+	
+	public Image getPlayerImage(){
+		
+		return playerImage;
+	}
+	
+	public Player(int x, int y, int width, int height, Image playerImage){
+
+		playerDimensions.x = x;
+		playerDimensions.y = y;
+		playerDimensions.width = width;
+		playerDimensions.height = height;
+		this.playerImage = playerImage;
+		
+	}
+	
+	public void updatePlayer(){
+		
+		if(Game.keys[KeyEvent.VK_A]){
+			playerDimensions.x--;
+		}	
+		if(Game.keys[KeyEvent.VK_D]){
+			playerDimensions.x++;
+		}
+		if(Game.keys[KeyEvent.VK_W]){
+			playerDimensions.y--;
+		}
+		if(Game.keys[KeyEvent.VK_S]){
+			playerDimensions.y++;
+		}
+		
+	}	
+	
+}
