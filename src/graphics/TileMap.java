2f03ae7b9324a533b93df9b1aa1d83afd910e9af
nocturnaltortoise <simonturner34@gmail.com>
2014-04-27 01:29:27 +0100

    adding tilemap file

---
+++ src/graphics/TileMap.java
@@ -0,0 +1,16 @@
+package graphics;
+
+public class TileMap {
+	
+	public char[] roomOne = {'x','d','x',
+							'x','s','x',
+							'x','s','x',
+							'x','s','x',
+							'x','s','x',
+							'x','s','x',
+							'x','s','x',};
+	
+	
+	
+	
+}
